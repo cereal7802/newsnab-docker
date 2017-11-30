5bc242030c186619708f7b96f4cae8a5dba0f511
Phill <cereal7802@gmail.com>
2017-11-30 05:45:11 -0600

    Tweaks to run

--- build/webserver/newznab-setup.sh
+++ build/webserver/newznab-setup.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 if [[ -z "$SVNUSER" || -z "$SVNPASS" ]]; then
-  echo "$(date) - Please set the SVNUSER and the SVNPASS environment vars for the container." >> /var/log/newznab/setup.log
+  echo "$(date) - Please set the SVNUSER and the SVNPASS environment vars for the container." 
   exit 1
 fi
 
-if [ -f /var/www/newznab/notouch ]; then
-  echo "$(date) - notouch file was found. newznab-setup.sh exited." >> /var/log/newznab/setup.log
-  exit
+if ! [ -e "/var/www/newznab/notouch" ]; then
+  echo "$(date) - notouch file does not exist. Continueing newznab setup."
+else
+  echo "$(date) - notouch file was found. newznab-setup.sh exited."
+  exit 0
 fi
 
 chown -R ${APACHE_RUN_USER}:${APACHE_RUN_USER} /var/www/newznab
 
-/usr/bin/svn co --username "$SVNUSER" --password "$SVNPASS" --non-interactive svn://svn.newznab.com/nn/branches/nnplus /var/www/newznab/
+svn co --username "$SVNUSER" --password "$SVNPASS" --no-auth-cache svn://svn.newznab.com/nn/branches/nnplus /var/www/newznab/
 
 cat > /var/www/newznab/www/config.php << EOF
 <?php
@@ -23,18 +25,18 @@ cat > /var/www/newznab/www/config.php << EOF
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'database');
 define('DB_PORT', 3306);
-define('DB_USER', 'root');
-define('DB_PASSWORD','newznab');
-define('DB_NAME', 'newznab');
+define('DB_USER', '${MYSQL_USER}');
+define('DB_PASSWORD','${MYSQL_PASSWORD}');
+define('DB_NAME', '${MYSQL_DATABASE}');
 define('DB_INNODB', false);
 define('DB_PCONNECT', true);
 define('DB_ERRORMODE', PDO::ERRMODE_SILENT);
 
-define('NNTP_USERNAME', 'xxxxxxx');
-define('NNTP_PASSWORD', 'xxxxxxx');
-define('NNTP_SERVER', 'reader.usenetbucket.com');
-define('NNTP_PORT', '443');
-define('NNTP_SSLENABLED', true);
+define('NNTP_USERNAME', '${NNTP_USERNAME}');
+define('NNTP_PASSWORD', '${NNTP_PASS}');
+define('NNTP_SERVER', '${NNTP_SERVER}');
+define('NNTP_PORT', '${NNTP_PORT}');
+define('NNTP_SSLENABLED', ${NNTP_SSL});
 
 define('CACHEOPT_METHOD', 'memcache');
 define('CACHEOPT_TTLFAST', '120');
@@ -55,7 +57,5 @@ chmod -R 777 /var/www/newznab/www/lib/smarty/templates_c
 chmod -R 777 /var/www/newznab/www/covers
 chmod -R 777 /var/www/newznab/www/install
 chmod -R 777 /var/www/newznab/nzbfiles
-
-
 
 touch /var/www/newznab/notouch
